64ede13ba6dd22536e32982291a7e8e419dcbe07
Nishanth Messi <nisanthrtts@gmail.com>
2023-06-23 22:13:57 +0530

    minor changes

--- models/movie.model.js
+++ models/movie.model.js
@@ -1,51 +1,58 @@
-const mongoose = require('mongoose'); 
+const mongoose = require('mongoose')
 
 /**
  * Define the schema of the movie resource to be stored in the db
  */
-const movieSchema = new mongoose.Schema({
+const movieSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: true,
-        minLength: 2
+      type: String,
+      required: true,
+      minLength: 2,
     },
     description: {
-        type: String,
-        required: true,
-        minLength: 5
+      type: String,
+      required: true,
+      minLength: 5,
     },
     casts: {
-        type: [String],
-        required: true
+      type: [String],
+      required: true,
     },
     imageUrl: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     trailerUrl: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     language: {
-        type: String,
-        required: true,
-        default: "English"
+      type: String,
+      required: true,
+      default: 'English',
     },
     releaseDate: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     director: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
+    },
+    producer: {
+      type: String,
+      required: true,
     },
     releaseStatus: {
-        type: String,
-        required: true,
-        default: "RELEASED",
+      type: String,
+      required: true,
+      default: 'RELEASED',
     },
-}, {timestamps: true});
+  },
+  { timestamps: true }
+)
 
-const Movie = mongoose.model('Movie', movieSchema); // creates a new model
+const Movie = mongoose.model('Movie', movieSchema) // creates a new model
 
-module.exports = Movie; // returning the model 
+module.exports = Movie // returning the model
